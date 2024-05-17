49d3c91781044a9b10820729d334ca1d74519d01
Chunli Tang <charlietang0326@gmail.com>
2024-05-17 16:15:41 -0500

    ppms

--- GUI/QDesign/PPMS.py
+++ GUI/QDesign/PPMS.py
@@ -12,7 +12,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 import random
-# import MultiPyVu as mpv # Uncommented it on the sever computer
+import MultiPyVu as mpv # Uncommented it on the/thesever computer
 import sys
 import Data_Processing_Suite.GUI.Icon as Icon
 
@@ -71,8 +71,7 @@ class PPMS(QWidget):
                                                 }
                                                 """)
         #  ---------------------------- PART 2 --------------------------------
-        # This command needs to be uncommented on the server PC
-        # self.server()  # Uncommented it on the sever computer
+
         ppms_connection = QHBoxLayout()
         connection_group_box = QGroupBox("Device Connection")
         # Set the layout for the group box
@@ -110,19 +109,16 @@ class PPMS(QWidget):
         ppms_connection.addWidget(self.host_label,1)
         ppms_connection.addWidget(self.host_entry_box, 2)
         ppms_connection.addStretch(1)
-        # self.refresh_gpib_list()
         ppms_connection.addWidget(self.port_label, 1)
         ppms_connection.addWidget(self.port_entry_box, 2)
         ppms_connection.addStretch(1)
         ppms_connection.addWidget(self.server_btn, 1)
         ppms_connection.addWidget(self.connect_btn, 1)
-        # combo_layout.setContentsMargins(50, 0, 50, 0)
         connection_group_box.setLayout(ppms_connection)
 
         #  ---------------------------- PART 3 --------------------------------
         temperature_reading_group_box = QGroupBox("Temperature (K)")  # Container widget for the horizontal layout
         temperature_main_layout = QVBoxLayout()
-        # voltage_reading_widget.setStyleSheet("QWidget { border: 2px solid black; }")
         temp_reading_layout = QHBoxLayout()
         self.cur_temp_Label = QLabel("Current Temperature:")
         self.cur_temp_Label.setFont(font)
@@ -145,7 +141,6 @@ class PPMS(QWidget):
         #  ---------------------------- PART 4 --------------------------------
         Field_reading_group_box = QGroupBox("Field (Oe)")  # Container widget for the horizontal layout
         Field_main_layout = QVBoxLayout()
-        # voltage_reading_widget.setStyleSheet("QWidget { border: 2px solid black; }")
         Field_reading_layout = QHBoxLayout()
         self.cur_field_Label = QLabel("Current Field:")
         self.cur_field_Label.setFont(font)
@@ -168,7 +163,6 @@ class PPMS(QWidget):
         #  ---------------------------- PART 5 --------------------------------
         Rot_reading_group_box = QGroupBox("Rotator (deg)")  # Container widget for the horizontal layout
         Rot_main_layout = QVBoxLayout()
-        # voltage_reading_widget.setStyleSheet("QWidget { border: 2px solid black; }")
         Rot_reading_layout = QHBoxLayout()
         self.cur_Rot_Label = QLabel("Current Degree:")
         self.cur_Rot_Label.setFont(font)
@@ -191,7 +185,6 @@ class PPMS(QWidget):
         #  ---------------------------- PART 6 --------------------------------
         Chamb_reading_group_box = QGroupBox("Chamber Status")  # Container widget for the horizontal layout
         Chamb_main_layout = QVBoxLayout()
-        # voltage_reading_widget.setStyleSheet("QWidget { border: 2px solid black; }")
         self.cur_chamb_status_reading_Label = QLabel("Unknown")
         self.cur_chamb_status_reading_Label.setFont(font)
         Chamb_main_layout.addWidget(self.cur_chamb_status_reading_Label)
@@ -200,7 +193,6 @@ class PPMS(QWidget):
         #  ---------------------------- PART 7 --------------------------------
         temperature_setting_group_box = QGroupBox("Set Temperature")  # Container widget for the horizontal layout
         temperature_set_main_layout = QVBoxLayout()
-        # voltage_reading_widget.setStyleSheet("QWidget { border: 2px solid black; }")
         temp_value_setting_layout = QHBoxLayout()
         self.set_temp_Label = QLabel("Target Temperature:")
         self.set_temp_Label.setFont(font)
@@ -211,10 +203,8 @@ class PPMS(QWidget):
         self.temp_validator.setNotation(QDoubleValidator.Notation.StandardNotation)
         #
         # # Set the validator to the QLineEdit
-        # self.cur_temp_entry_box.setValidator(validator)
         # Set the validator to the QLineEdit
         self.cur_temp_entry_box.setValidator(self.temp_validator)
-        # self.cur_temp_entry_box.setValidator(IntegerValidator(1.8, 400.0, 1))
         self.cur_temp_entry_box.setPlaceholderText("Enter an temperature between 1.8 and 400")
         self.set_temp_unit_Label = QLabel("K")
         self.set_temp_unit_Label.setFont(font)
@@ -230,7 +220,6 @@ class PPMS(QWidget):
         self.temp_rate_validator = QDoubleValidator(0, 50.0, 2)
         self.temp_rate_validator.setNotation(QDoubleValidator.Notation.StandardNotation)
         self.temp_rate_entry_box.setValidator(self.temp_rate_validator)
-        # self.temp_rate_entry_box.setValidator(IntegerValidator(0, 50,1))
         self.temp_rate_entry_box.setPlaceholderText("Enter an rate between 1 and 50")
         self.temp_rate_unit_Label = QLabel("K/s")
         self.temp_rate_unit_Label.setFont(font)
@@ -286,7 +275,6 @@ class PPMS(QWidget):
         #  ---------------------------- PART 8 --------------------------------
         Field_setting_group_box = QGroupBox("Set Field")  # Container widget for the horizontal layout
         Field_set_main_layout = QVBoxLayout()
-        # voltage_reading_widget.setStyleSheet("QWidget { border: 2px solid black; }")
         field_value_setting_layout = QHBoxLayout()
         self.set_field_Label = QLabel("Target Temperature:")
         self.set_field_Label.setFont(font)
@@ -295,7 +283,6 @@ class PPMS(QWidget):
         self.field_validator = QDoubleValidator(-90000.00, 90000.0, 2)
         self.field_validator.setNotation(QDoubleValidator.Notation.StandardNotation)
         self.cur_field_entry_box.setValidator(self.field_validator)
-        # self.cur_field_entry_box.setValidator(IntegerValidator(-90000, 90000, 1))
         self.cur_field_entry_box.setPlaceholderText("Enter a field between -90000 and 90000")
         self.set_field_unit_Label = QLabel("Oe")
         self.set_field_unit_Label.setFont(font)
@@ -311,7 +298,6 @@ class PPMS(QWidget):
         self.field_rate_validator = QDoubleValidator(0, 220.0, 2)
         self.field_rate_validator.setNotation(QDoubleValidator.Notation.StandardNotation)
         self.field_rate_entry_box.setValidator(self.field_rate_validator)
-        # self.field_rate_entry_box.setValidator(IntegerValidator(0, 220, 1))
         self.field_rate_entry_box.setPlaceholderText("Enter an rate between 0 and 220")
         self.field_rate_unit_Label = QLabel("Oe/s")
         self.field_rate_unit_Label.setFont(font)
@@ -545,31 +531,30 @@ class PPMS(QWidget):
                                                        """)
 
     def start_server(self):
-        # self.server()
-        # self.client = mpv.Client(host=self.host_entry_box.displayText())
         if self.server_btn_clicked == False:
-            # self.s.open()  # Uncommented it on the sever computer
+            self.server()
             self.server_btn.setText('Stop Server')
             self.server_btn_clicked = True
             self.connect_btn.setEnabled(True)
         elif self.server_btn_clicked == True:
-            # self.s.close()  # Uncommented it on the sever computer
+            self.s.close()  # Uncommented it on the sever computer
             self.server_btn.setText('Start Server')
             self.server_btn_clicked = False
             self.connect_btn.setEnabled(False)
 
     # Uncommented it on the sever computer
-    # def server(self, flags: str = ''):
-    #     user_flags = []
-    #     if flags == '':
-    #         user_flags = sys.argv[1:]
-    #     else:
-    #         user_flags = flags.split(' ')
-    #
-    #     self.s = mpv.Server(user_flags, keep_server_open=True)
-    #     self.s.open()
+    def server(self, flags: str = ''):
+        user_flags = []
+        if flags == '':
+            user_flags = sys.argv[1:]
+        else:
+            user_flags = flags.split(' ')
+
+        self.s = mpv.Server(user_flags, keep_server_open=True)
+        self.s.open()  # Uncommented it on the sever computer
 
     def connect_client(self):
+        self.client = mpv.Client(host_entry_box=self.host_entry_box.displayText())
         self.reading_timer = QTimer()
         if self.connect_btn_clicked == False:
             self.connect_btn.setText('Stop Client')
@@ -579,24 +564,24 @@ class PPMS(QWidget):
             # with mpv.Client(self.host, self.port) as self.client:
 
             self.client.open()
-
-            #     time.sleep(5)
-            #     self.reading_timer = QTimer()
-            #     self.reading_timer.setInterval(1000)
-            #     self.reading_timer.timeout.connect(self.ppms_reading)
-            #     self.reading_timer.start()
-
-            """
-            THis is only for test purpose
-            """
-
+            import time
+            time.sleep(5)
+            self.reading_timer = QTimer()
             self.reading_timer.setInterval(1000)
             self.reading_timer.timeout.connect(self.ppms_reading)
             self.reading_timer.start()
 
+            # """
+            # THis is only for test purpose
+            # """
+            #
+            # self.reading_timer.setInterval(1000)
+            # self.reading_timer.timeout.connect(self.ppms_reading)
+            # self.reading_timer.start()
+
 
         elif self.connect_btn_clicked == True:
-            # self.client.close_client()
+            self.client.close_client()
             self.connect_btn.setText('Start Client')
             self.connect_btn_clicked = False
             self.server_btn.setEnabled(True)
@@ -609,28 +594,28 @@ class PPMS(QWidget):
 
     def ppms_reading(self):
         # Uncomment this section to enable ppms control
-        # T, sT = self.client.get_temperature()
-        # F, sF = self.client.get_field()
+        T, sT = self.client.get_temperature()
+        F, sF = self.client.get_field()
         R, sR = "N/A", "Unknown"
-        # C = self.client.get_chamber()
-        # self.cur_temp_reading_Label.setText(f'{T} K')
-        # self.cur_temp_status_reading_Label.setText(f'{sT}')
-        # self.cur_field_reading_Label.setText(f'{F} Oe')
-        # self.cur_field_status_reading_Label.setText(f'{sF}')
-        # self.cur_chamb_status_reading_Label.setText(f'{C}')
-
-        """
-        This is only for test purpose
-        """
-        T, F = random.randint(2, 400), random.randint(-10000, 10000)
-        sF, sT, C = "Testing!","Testing!","Testing!"
+        C = self.client.get_chamber()
         self.cur_temp_reading_Label.setText(f'{T} K')
         self.cur_temp_status_reading_Label.setText(f'{sT}')
         self.cur_field_reading_Label.setText(f'{F} Oe')
         self.cur_field_status_reading_Label.setText(f'{sF}')
-        self.cur_Rot_reading_Label.setText(f'{R} degs')
-        self.cur_Rot_status_reading_Label.setText(f'{sR}')
         self.cur_chamb_status_reading_Label.setText(f'{C}')
+
+        # """
+        # This is only for test purpose
+        # """
+        # T, F = random.randint(2, 400), random.randint(-10000, 10000)
+        # sF, sT, C = "Testing!","Testing!","Testing!"
+        # self.cur_temp_reading_Label.setText(f'{T} K')
+        # self.cur_temp_status_reading_Label.setText(f'{sT}')
+        # self.cur_field_reading_Label.setText(f'{F} Oe')
+        # self.cur_field_status_reading_Label.setText(f'{sF}')
+        # self.cur_Rot_reading_Label.setText(f'{R} degs')
+        # self.cur_Rot_status_reading_Label.setText(f'{sR}')
+        # self.cur_chamb_status_reading_Label.setText(f'{C}')
 
     def setTemp(self):
         self.set_temp = self.cur_temp_entry_box.displayText()
