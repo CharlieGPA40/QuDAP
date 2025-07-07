f7d41dfbd461be1ee069ee849730171f5014b31f
CharlieGPA40 <tcl951481023@gmail.com>
2025-07-06 22:45:27 -0700

    Update test_gui.py

--- QuDAP/test_gui.py
+++ QuDAP/test_gui.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
 import initalization
-
+import os
+os.environ["QT_QPA_PLATFORM"] = "offscreen"
 
 def test_startup(qtbot):
-
-	app = initalization.main(test_mode=True)
-	qtbot.addWidget(app)
+	communicator = Communicator()
+	window = MainWindow(communicator)
+	qtbot.addWidget(window)
+	window.show()  # Explicitly show the window
+	assert window.isVisible()  # Check if it's visible
+	assert window.windowTitle() == "QuDAP"
